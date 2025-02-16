99e05a21b5de172ad510cda05b6fc7338da9de9b
Kevin Exton <kcexton01@gmail.com>
2025-02-16 22:39:29 +0000

    Updated the license to the Affero License.

--- src/controller/control_connector.cpp
+++ src/controller/control_connector.cpp
@@ -3,14 +3,14 @@
 *   This file is part of Cloudbus.
 *
 *   Cloudbus is free software: you can redistribute it and/or modify it under the 
-*   terms of the GNU General Public License as published by the Free Software 
+*   terms of the GNU Affero General Public License as published by the Free Software 
 *   Foundation, either version 3 of the License, or any later version.
 *
 *   Cloudbus is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
 *   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-*   See the GNU General Public License for more details.
+*   See the GNU Affero General Public License for more details.
 *
-*   You should have received a copy of the GNU General Public License along with Cloudbus. 
+*   You should have received a copy of the GNU Affero General Public License along with Cloudbus. 
 *   If not, see <https://www.gnu.org/licenses/>. 
 */
 #include "control_connector.hpp"

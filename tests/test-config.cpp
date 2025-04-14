8636ccba54e112a326d3988dec71f0261d7c8e81
Kevin Exton <kcexton01@gmail.com>
2025-04-14 04:54:53 +0000

    Removed a dependency on a test input file, and made my one test a bit more comprehensive so that it is actually testing something.

--- tests/test-config.cpp
+++ tests/test-config.cpp
@@ -1,18 +1,37 @@
 #include "tests.hpp"
 #include "../src/config.hpp"
 #include <string>
-#include <fstream>
+#include <sstream>
 #include <iostream>
 
 static int test_ingest_config(const std::string& path){
     using namespace cloudbus;
-    if(std::fstream f{path, f.in}){
-        config::configuration conf;
-        f >> conf;
-        return TEST_PASS;
+    config::configuration conf;
+    std::stringstream ss;
+    ss << "[Cloudbus]\n"
+        << "[Test Service]\n"
+        << "bind=unix:///var/run/test.sock\n"
+        << "backend=unix:///var/run/backend.sock\n";
+    ss >> conf;
+    for(auto& section: conf.sections()){
+        if(section.heading == "Cloudbus"){
+            if(!section.config.empty())
+                return TEST_FAIL;
+        } else if (section.heading == "Test Service"){
+            if(section.config.empty())
+                return TEST_FAIL;
+            for(auto&[k,v]: section.config){
+                if(k == "bind") {
+                    if(v != "unix:///var/run/test.sock")
+                        return TEST_FAIL;
+                } else if (k == "backend"){
+                    if(v != "unix:///var/run/backend.sock")
+                        return TEST_FAIL;
+                } else return TEST_FAIL;
+            }
+        } else return TEST_FAIL;
     }
-    std::cerr << "Invalid configuration file in test_ingest_config()." << std::endl;
-    return TEST_ERROR;
+    return TEST_PASS;
 }
 int main(int argc, char **argv){
     int rc = TEST_PASS;
